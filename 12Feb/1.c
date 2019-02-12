ef27f9bea5c8ebf8298d10ed79053dd1c80d0cc8
s10singh97 <s10singh97@yahoo.in>
2019-02-12 11:05:23 +0530

    MST Prim's Algo

--- 12Feb/1.c
+++ 12Feb/1.c
@@ -1,1 +1,61 @@
-// Todays
+// Prim's MST
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<limits.h>
+#define V 6
+
+int minc(int key[], bool vnotinc[])
+{
+    int min = INT_MAX, min_ind;
+    for(int i = 0; i < V; i++)
+    {
+        if(vnotinc[i] == false && key[i] < min)
+        {
+            min = key[i];
+            min_ind = i;
+        }
+    }
+    return min_ind;
+}
+
+void prim(int graph[V][V])
+{
+    int parent[V];
+    int key[V];
+    bool vnotinc[V];
+    for(int i = 0; i < V; i++)
+    {
+        key[i] = INT_MAX;
+        vnotinc[i] = false;
+    }
+    key[0] = 0;
+    parent[0] = -1;
+    for(int j = 0; j < V-1; j++)
+    {
+        int a = minc(key, vnotinc);
+        vnotinc[a] = true;
+        for(int j = 0; j < V; j++)
+        {
+            if(graph[a][j] && vnotinc[j] == false && graph[a][j] < key[j])
+            {
+                parent[j] = a;
+                key[j] = graph[a][j];
+            }
+        }
+    }
+    
+    printf("Edge \tWeight\n"); 
+    for (int i = 1; i < V; i++) 
+    printf("%c - %c \t%d \n", parent[i]+65, i+65, graph[i][parent[i]]);
+}
+
+int main()
+{
+    int graph[V][V] = {
+        {0,5,0,0.0,4},{5,0,2,0,0,10},{0,2,0,5,6,12},{0,0,5,0,3,0},{0,0,6,3,0,1},{4,10,12,0,1,0}
+    };
+    prim(graph);
+    return 0;
+}
